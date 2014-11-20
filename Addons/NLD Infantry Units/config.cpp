d5d415e83ab7ba76d77775c4898735a85d5b7c75
ancorb1974 <ancorb1974@live.nl>
2014-11-20 19:07:37 +0100

    uav backpack uim pictures changed

--- Addons/NLD Infantry Units/config.cpp
+++ Addons/NLD Infantry Units/config.cpp
@@ -2298,7 +2298,7 @@ class CfgVehicles {
 	
 	class NLD_DST_Carryall_UAV : B_UAV_01_backpack_F {
 		displayName = "NLD Desert Carryall UAV";
-		picture = "\NLD Infantry Units\Units\Desert\Data\uim\Backpack.jpg";
+		picture = "\NLD Infantry Units\Units\NFP-Tan\Data\uim\Backpack-UAV.jpg";
 		hiddenSelectionsTextures[] = {"\NLD Infantry Units\Units\NFP-Tan\Data\Backpack-UAV.paa"};
 
 		class TransportMagazines
@@ -2340,31 +2340,31 @@ class CfgVehicles {
 	
 	class NLD_WLD_Carryall_UAV : NLD_DST_Carryall_UAV {
 		displayName = "NLD Woodland Carryall UAV";
-		picture = "\NLD Infantry Units\Units\Woodland\Data\uim\Backpack.jpg";
+		picture = "\NLD Infantry Units\Units\Woodland\Data\uim\Backpack-UAV.jpg";
 		hiddenSelectionsTextures[] = {"\NLD Infantry Units\Units\Woodland\Data\Backpack-UAV.paa"};
 	};
 
 	class NLD_NFPT_Carryall_UAV : NLD_DST_Carryall_UAV {
 		displayName = "NLD NFP-Tan Carryall UAV";
-		picture = "\NLD Infantry Units\Units\NFP-Tan\Data\uim\Backpack.jpg";
+		picture = "\NLD Infantry Units\Units\NFP-Tan\Data\uim\Backpack-UAV.jpg";
 		hiddenSelectionsTextures[] = {"\NLD Infantry Units\Units\NFP-Tan\Data\Backpack-UAV.paa"};
 	};
 
 	class NLD_NFPG_Carryall_UAV : NLD_DST_Carryall_UAV {
 		displayName = "NLD NFP-Green Carryall UAV";
-		picture = "\NLD Infantry Units\Units\NFP-Green\Data\uim\Backpack.jpg";
+		picture = "\NLD Infantry Units\Units\NFP-Green\Data\uim\Backpack-UAV.jpg";
 		hiddenSelectionsTextures[] = {"\NLD Infantry Units\Units\NFP-Green\Data\Backpack-UAV.paa.paa"};
 	};
 
 	class NLD_WTR_Carryall_UAV : NLD_DST_Carryall_UAV {
 		displayName = "NLD Winter Carryall UAV";
-		picture = "\NLD Infantry Units\Units\Winter\Data\uim\Backpack.jpg";
+		picture = "\NLD Infantry Units\Units\Woodland\Data\uim\Backpack-UAV.jpg";
 		hiddenSelectionsTextures[] = {"\NLD Infantry Units\Units\Woodland\Data\Backpack-UAV.paa"};
 	};
 
 	class NLD_JGL_Carryall_UAV : NLD_DST_Carryall_UAV {
 		displayName = "NLD Jungle Carryall UAV";
-		picture = "\NLD Infantry Units\Units\Jungle\Data\uim\Backpack.jpg";
+		picture = "\NLD Infantry Units\Units\NFP-Tan\Data\uim\Backpack-UAV.jpg";
 		hiddenSelectionsTextures[] = {"\NLD Infantry Units\Units\NFP-Tan\Data\Backpack-UAV.paa"};
 	};
 
