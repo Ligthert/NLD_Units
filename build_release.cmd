8c4e3e6b34d52dfc60426a80266decfe882baa06
Sacha Ligthert <sacha@ligthert.net>
2015-02-07 03:16:41 +0100

    Update buildscripts

--- build_release.cmd
+++ build_release.cmd
@@ -19,7 +19,7 @@ xcopy *.* %DEST% /E
 
 
 REM Delete what we didn't need
-DEL /S /Q %DEST%\*.md %DEST%\*.cmd %DEST%\*.gitignore %DEST%\Addons\x_makepbo.bat
+DEL /S /Q %DEST%\*.md %DEST%\*.cmd %DEST%\*.gitignore %DEST%\Addons\x_makepbo.bat %DEST%\.synqinfo
 RD /S /Q %DEST%\NLD_Units_Showcase.VR
 
 
