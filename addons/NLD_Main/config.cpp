817f236d3b0bb50ecf80feffe1444b0e6c3621f8
Sacha Ligthert <sacha@ligthert.net>
2015-02-07 02:06:34 +0100

    Added Zeus compatibility
    Don't know how I did it...

---
+++ addons/NLD_Main/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches {
+	class NLD_Units_Main {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Characters_F_BLUFOR"};
+	};
+};
+
+class CfgFactionClasses
+{
+	class NLD_Units
+	{
+		displayName="NLD Armed Forces";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+};
