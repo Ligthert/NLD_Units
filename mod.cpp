29713ad23c1d9b284367193224db54af6e91f4a0
Sacha Ligthert <sacha@ligthert.net>
2015-02-08 22:20:35 +0100

    Nieuwe release!

--- mod.cpp
+++ mod.cpp
@@ -2,5 +2,5 @@ name = "NLD Units: Dutch Units for ArmA 3";
 picture = "mod.jpg";
 actionName = "Website";
 action = "http://lowtac.nl";
-description = "NLD Units v0.1.1";
+description = "NLD Units v0.1.5";
 author = "Lowlands Tactical";
