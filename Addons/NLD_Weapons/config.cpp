0102857063f4ab0be1ecd8166bce4b5e13ede9e6
Sacha Ligthert <sacha@ligthert.net>
2015-02-17 01:02:11 +0100

    CUP

---
+++ Addons/NLD_Weapons/config.cpp
@@ -0,0 +1,127 @@
+class CfgPatches {
+	class NLD_Units_Weapons {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"NLD_Units_Infantry"};
+	};
+};
+
+// Weapons
+class CfgWeapons {
+
+	// ext class ref
+	class CUP_arifle_M4A1_black;
+	class CUP_arifle_M4A1_BUIS_GL;
+	class CUP_lmg_M249_para;
+	class CUP_lmg_m240;
+
+	// Dimaco C8
+	class NLD_blk_C8: CUP_arifle_M4A1_black {
+		displayName="Dimaco C8";
+		author="Lowlands Tactical";
+		scope=2;
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot = "CUP_PicatinnyTopMountM4";
+				item = "CUP_optic_ACOG";
+			};
+			class LinkedItemsAcc
+			{
+				slot = "CUP_PicatinnySideMountM4";
+				item = "CUP_acc_ANPEQ_2_grey";
+			};
+		};
+	};
+
+	// Dimaco C8 GL
+	class NLD_blk_C8GL: CUP_arifle_M4A1_BUIS_GL {
+		displayName="Dimaco C8";
+		author="Lowlands Tactical";
+		scope=2;
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot = "CUP_PicatinnyTopMountM4";
+				item = "CUP_optic_ACOG";
+			};
+			class LinkedItemsAcc
+			{
+				slot = "CUP_PicatinnySideMountM4";
+				item = "CUP_acc_ANPEQ_2_grey";
+			};
+		};
+	};
+
+	// Minimi
+	class NLD_Minimi: CUP_lmg_M249_para {
+		displayName="FN Minimi";
+		author="Lowlands Tactical";
+		scope=2;
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot = "CUP_PicatinnyTopMountM249";
+				item = "CUP_optic_ElcanM145";
+			};
+			class LinkedItemsAcc
+			{
+				slot = "CUP_PicatinnySideMountM249";
+				item = "CUP_acc_ANPEQ_2_grey";
+			};
+		};
+	};
+
+	// Mag
+	class NLD_MAG: CUP_lmg_M240 {
+		displayName="FN MAG";
+		author="Lowlands Tactical";
+		scope=2;
+		magazines[] =
+		{
+			"CUP_100Rnd_TE4_LRT4_White_Tracer_762x51_Belt_M",
+			"CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M",
+			"NLD_200Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M"
+		};
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot = "CUP_PicatinnyTopMountM240";
+				item = "CUP_optic_ElcanM145";
+			};
+			class LinkedItemsAcc
+			{
+				slot = "CUP_PicatinnySideMountM240";
+				item = "CUP_acc_ANPEQ_2_grey";
+			};
+		};
+	};
+};
+
+class CfgMagazines {
+
+	// ext class ref
+	class CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M;
+
+	// 200rnd belt
+	class NLD_200Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M: CUP_100Rnd_TE4_LRT4_Red_Tracer_762x51_Belt_M {
+		author = "CUP";
+		scope = 2;
+		displayName = "7.62x51mm 200 Rnd 240 Belt (red tracers)";
+		picture = "\A3\Weapons_F\Data\UI\M_200Rnd_65x39_CA.paa";
+		model = "\A3\weapons_F\ammo\mag_univ.p3d";
+		count = 200;
+		type = 2 * 256;
+		ammo = "B_762x51_Tracer_Red";
+		initSpeed = 900;
+		tracersEvery = 4;
+		lastRoundsTracer = 4;
+		nameSound = "mgun";
+		descriptionShort = "7.62x51mm Belt (red tracers)";
+	};
+};
