5a9b06f0b65d5d7074b56fd43c8a63f5ce3057cd
Sacha Ligthert <sacha@ligthert.net>
2015-03-31 00:52:34 +0200

    Added insignia (11AMB+KCT)

---
+++ Addons/NLD_Insignia/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches {
+	class NLD_Units_Insignia {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"NLD_Infantry_Units"};
+	};
+};
+
+class CfgUnitInsignia
+{
+    class NLD_11AMB
+    {
+        displayName = "11AMB";
+        author = "Lowlands Tactical";
+        texture = "\NLD_Infantry_Units\Badges\11LMB.paa";
+        textureVehicle = "\NLD_Infantry_Units\Badges\11LMB.paa";
+    };
+    class NLD_KCT
+    {
+    	displayName = "KCT";
+    	author = "Lowlands Tactical";
+    	texture = "\NLD_Infantry_Units\Badges\KCT.paa";
+    	textureVehicle = "\NLD_Infantry_Units\Badges\KCT.paa";
+    };
+};
