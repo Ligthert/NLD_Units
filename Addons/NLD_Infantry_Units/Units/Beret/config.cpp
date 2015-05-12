48e1829be6a6ea26a84032329db9dbdbbc120e8f
Ancor Baumhöer <ancorb1974@live.nl>
2015-05-12 21:36:04 +0200

    Pictures & Insignias
    Replaced Royal Army beret with Royal Marines beret
    Changed all pictures jpg to paa
    changed & added Insignias

--- Addons/NLD_Infantry_Units/Units/Beret/config.cpp
+++ Addons/NLD_Infantry_Units/Units/Beret/config.cpp
@@ -49,14 +49,14 @@ class cfgWeapons
 	class InventoryItem_Base_F;
 	class HeadgearItem;
 
-	class Dutch_Beret_RoyalArmy: ItemCore
+	class Dutch_Beret_RoyalMarines: ItemCore
 	{
 		scope = 2;
 		weaponPoolAvailable = 1;
-		displayName = "Koninklijke Landmacht";
-		picture = "\NLD_Infantry_Units\Units\Beret\uim\RoyalArmy.jpg";
+		displayName = "Koninklijke Marine";
+		picture = "\NLD_Infantry_Units\Units\Beret\uim\RoyalMarines.paa";
 		model = "\A3\Characters_F\Common\headgear_beret01";
-		hiddenSelectionsTextures[] = {"\NLD_Infantry_Units\Units\Beret\RoyalArmy.paa"};
+		hiddenSelectionsTextures[] = {"\NLD_Infantry_Units\Units\Beret\RoyalMarines.paa"};
 		hiddenSelections[] = {"Camo"};
          
 		class ItemInfo: HeadgearItem
@@ -66,7 +66,7 @@ class cfgWeapons
 			modelSides[] = {3,1};
 			armor = "3*0.5";
 			passThrough = 0.95;
-			hiddenSelectionsTextures[] = {"\NLD_Infantry_Units\Units\Beret\RoyalArmy.paa"};
+			hiddenSelectionsTextures[] = {"\NLD_Infantry_Units\Units\Beret\RoyalMarines.paa"};
 			hiddenSelections[] = {"Camo"};
 		};
 	};
@@ -76,7 +76,7 @@ class cfgWeapons
 		scope = 2;
 		weaponPoolAvailable = 1;
 		displayName = "Korps Commandotroepen";
-		picture = "\NLD_Infantry_Units\Units\Beret\uim\Commandos.jpg";
+		picture = "\NLD_Infantry_Units\Units\Beret\uim\Commandos.paa";
 		model = "\A3\Characters_F\Common\headgear_beret01";
 		hiddenSelectionsTextures[] = {"\NLD_Infantry_Units\Units\Beret\Commandos.paa"};
 		hiddenSelections[] = {"Camo"};
@@ -98,7 +98,7 @@ class cfgWeapons
 		scope = 2;
 		weaponPoolAvailable = 1;
 		displayName = "Luchtmobiele Brigade";
-		picture = "\NLD_Infantry_Units\Units\Beret\uim\11AirManoeuvreBrigade.jpg";
+		picture = "\NLD_Infantry_Units\Units\Beret\uim\11AirManoeuvreBrigade.paa";
 		model = "\A3\Characters_F\Common\headgear_beret01";
 		hiddenSelectionsTextures[] = {"\NLD_Infantry_Units\Units\Beret\11AirManoeuvreBrigade.paa"};
 		hiddenSelections[] = {"Camo"};
