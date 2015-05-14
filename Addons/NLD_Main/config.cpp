9e4a362bbf3b5f947f32098ddb1a01790c669730
Sacha Ligthert <outcast@overseer.ligthert.net>
2015-05-14 19:50:03 +0200

    This error is the bane of my existence...

---
+++ Addons/NLD_Main/config.cpp
@@ -0,0 +1,71 @@
+class CfgPatches {
+	class NLD_Units_Main {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Characters_F_BLUFOR"};
+	};
+};
+
+class CfgFactionClasses
+{
+
+	class NLD_Units
+	{
+		displayName="NLD Armed Forces";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+
+/*
+	class NLD_Units_WLD
+	{
+		displayName="NLD Armed Forces (Woodland)";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+
+	class NLD_Units_DST
+	{
+		displayName="NLD Armed Forces (Desert)";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+
+	class NLD_Units_WTR
+	{
+		displayName="NLD Armed Forces (WTR)";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+
+	class NLD_Units_NFPG
+	{
+		displayName="NLD Armed Forces (NFP-Green)";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+
+	class NLD_Units_NFPT
+	{
+		displayName="NLD Armed Forces (NFP-Tan)";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+
+	class NLD_Units_JGL
+	{
+		displayName="NLD Armed Forces (Jungle)";
+		icon="\NLD_Infantry_Units\Icon\Icon.jpg";
+		priority=1;
+		side=1;
+	};
+*/
+	
+};
