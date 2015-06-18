07890496f3ea71a9a2c4f59efb0516e2727a3157
Ancor Baumhöer <ancorb1974@live.nl>
2015-06-18 20:09:57 +0200

    Changed MH65 for Lynx
    Changed MH65 for Lynx

--- Addons/NLD_Vehicle_Units/Air/config.cpp
+++ Addons/NLD_Vehicle_Units/Air/config.cpp
@@ -15,8 +15,8 @@ class RCWSOptics;
 
 class CfgVehicles 
 {
-	class O_Heli_Light_02_F;
-	class O_Heli_Light_02_unarmed_F;
+	class I_Heli_light_03_F;
+	class I_Heli_light_03_unarmed_F;
 	class Heli_Transport_02_base_F;
 	class LLW_Apache_RNLAF;
 	class NH90;
@@ -30,6 +30,219 @@ class CfgVehicles
 		crew = "NLD_HeliPilot";
 		faction = "NLD_Units";
 		vehicleClass = Air;
+	};
+
+	class NLD_Lynx : I_Heli_light_03_unarmed_F
+	{
+		author = "Lowlands Tactical";
+		scope = 2;
+		_generalMacro = "I_Heli_light_03_unarmed_F";
+		displayName = "Lynx";
+		side = 1;
+		crew = "NLD_HeliPilot";
+		faction = "NLD_Units";
+		vehicleClass = Air;
+		hiddenSelections[] = {"Camo1"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Air\Data\Lynx.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+	
+	class NLD_MH65_Armed : I_Heli_light_03_F
+	{
+		author = "Lowlands Tactical";
+		scope = 2;
+		_generalMacro = "I_Heli_light_03_F";
+		displayName = "Lynx (Armed)";
+		side = 1;
+		crew = "NLD_HeliPilot";
+		faction = "NLD_Units";
+		vehicleClass = Air;
+		hiddenSelections[] = {"Camo1"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Air\Data\Lynx.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+	
+	class NLD_NH90: NH90
+	{
+		author = "Aplion";
+		scope = 2;
+		_generalMacro = "NH90Marine";
+		displayName = "NH-90";
+		side = 1;
+		crew = "NLD_HeliPilot";
+		faction = "NLD_Units";
+		vehicleClass = Air;
+		hiddenSelections[] = {"camo1","camo2","camo3","camo4"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Air\Data\NH90\outerhaul_co.paa","\NLD_Vehicle_Units\Air\Data\NH90\upperTail_co.paa","\NLD_Vehicle_Units\Air\Data\NH90\misc_co.paa","\NLD_Vehicle_Units\Air\Data\NH90\doors_co.paa"};
+	};
+
+	/*
+    class NLD_NH90 : Heli_Transport_02_base_F
+    {
+		author = "Lowlands Tactical";
+		scope = 2;
+		_generalMacro = "Heli_Transport_02_base_F";
+		displayName = "NH-90";
+		side = 1;
+		crew = "NLD_HeliPilot";
+		faction = "NLD_Units";
+		vehicleClass = Air;
+	    hiddenSelections[]= {"camo1","camo2","camo3"};
+	    hiddenSelectionsTextures[]= {"\NLD_Vehicle_Units\Air\Data\NH90_1.paa","\NLD_Vehicle_Units\Air\Data\NH90_2.paa","\NLD_Vehicle_Units\Air\Data\NH90_3.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
 	};
 
 	class NLD_MH65 : O_Heli_Light_02_unarmed_F
@@ -114,89 +327,6 @@ class CfgVehicles
 		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Air\Data\MH65.paa"};
 		AGM_FastRoping = 1;
 		AGM_FastRoping_Positions[] = {{1.4, 1.4, 0.05}, {-1.4, 1.4, 0.05}};
-		
-		class TransportItems {
-			class _xx_FirstAidKit {
-				name = "FirstAidKit";
-				count = 10;
-			};
-		};
-		
-		class TransportMagazines
-		{
-			class _xx_1Rnd_HE_Grenade_shell
-			{
-				magazine="1Rnd_HE_Grenade_shell";
-				count=10;
-			};
-			class _xx_HandGrenade
-			{
-				magazine="HandGrenade";
-				count=5;
-			};
-			class _xx_SmokeShell
-			{
-				magazine="SmokeShell";
-				count=10;
-			};
-			class _xx_SmokeShellGreen
-			{
-				magazine="SmokeShellGreen";
-				count=5;
-			};
-			class _xx_30Rnd_65x39_caseless_mag
-			{
-				magazine="30Rnd_65x39_caseless_mag";
-				count=25;
-			};
-			class _xx_100Rnd_65x39_caseless_mag
-			{
-				magazine="100Rnd_65x39_caseless_mag";
-				count=10;
-			};
-			class _xx_200Rnd_65x39_cased_Box
-			{
-				magazine="200Rnd_65x39_cased_Box";
-				count=10;
-			};
-			class _xx_NLAW_F
-			{
-				magazine="NLAW_F";
-				count=5;
-			};
-		};
-		class TransportWeapons {};
-	};
-	
-	class NLD_NH90: NH90
-	{
-		author = "Aplion";
-		scope = 2;
-		_generalMacro = "NH90Marine";
-		displayName = "NH-90";
-		side = 1;
-		crew = "NLD_HeliPilot";
-		faction = "NLD_Units";
-		vehicleClass = Air;
-		hiddenSelections[] = {"camo1","camo2","camo3","camo4"};
-		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Air\Data\NH90\outerhaul_co.paa","\NLD_Vehicle_Units\Air\Data\NH90\upperTail_co.paa","\NLD_Vehicle_Units\Air\Data\NH90\misc_co.paa","\NLD_Vehicle_Units\Air\Data\NH90\doors_co.paa"};
-	};
-
-	/*
-    class NLD_NH90 : Heli_Transport_02_base_F
-    {
-		author = "Lowlands Tactical";
-		scope = 2;
-		_generalMacro = "Heli_Transport_02_base_F";
-		displayName = "NH-90";
-		side = 1;
-		crew = "NLD_HeliPilot";
-		faction = "NLD_Units";
-		vehicleClass = Air;
-	    hiddenSelections[]= {"camo1","camo2","camo3"};
-	    hiddenSelectionsTextures[]= {"\NLD_Vehicle_Units\Air\Data\NH90_1.paa","\NLD_Vehicle_Units\Air\Data\NH90_2.paa","\NLD_Vehicle_Units\Air\Data\NH90_3.paa"};
-	    AGM_FastRoping = 1;
-		AGM_FastRoping_Positions[] = {{1.4, -2.5, 0.}, {-1.4, -2.5, 0}};
 		
 		class TransportItems {
 			class _xx_FirstAidKit {
