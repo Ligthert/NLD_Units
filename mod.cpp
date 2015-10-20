bfb7675ef1972b8a434876556b01a8d72dab5e6f
ancorb1974 <ancorb1974@live.nl>
2015-10-20 15:59:52 +0200

    changed multiple text files

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,10 @@
 name = "NLD Units: Dutch Units for ArmA 3";
 picture = "NLD_Units.paa";
+logo = "NLD_Units.paa";
+logoOver = "NLD_Units_Overlay.paa";
+tooltipOwned = "@NLD_Units";
 actionName = "Website";
 action = "http://lowtac.nl";
-description = "NLD Units v20150802";
+overview = "NLD Units: Dutch Units for ArmA 3 v201510205.";
+description = "NLD Units v201510205";
 author = "Lowlands Tactical";
