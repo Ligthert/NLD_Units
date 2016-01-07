0264b2b306b4640176a93986234acec474aade29
ancorb1974 <ancorb1974@live.nl>
2016-01-07 14:04:44 +0100

    Added DAF4442 Trucks

--- Addons/NLD_Vehicle_Units/Wheeled/config.cpp
+++ Addons/NLD_Vehicle_Units/Wheeled/config.cpp
@@ -11,6 +11,13 @@ class CfgVehicles {
 	class MRAP_03_base_F;		// External class reference
 	class MRAP_03_hmg_base_F;	// External class reference
 	class MRAP_03_gmg_base_F;	// External class reference
+	class I_Truck_02_transport_F;
+	class I_Truck_02_covered_F;
+	class I_Truck_02_fuel_F;
+	class I_Truck_02_box_F;
+	class I_Truck_02_medical_F;
+	class I_Truck_02_ammo_F;
+
 	
 // Fennek - Desert
 	class NLD_DST_Fennek : MRAP_03_base_F {
@@ -257,4 +264,387 @@ class CfgVehicles {
 		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Fennek\winter\Fennek.paa", "\NLD_Vehicle_Units\Wheeled\Fennek\winter\turret.paa"};
 	};
 
+	class NLD_DAF_Transport: I_Truck_02_transport_F
+	{
+		author="Lowlands Tactical";
+		displayName="DAF 4442 (Transport)";
+		side=1;
+		scope=2;
+		faction="NLD_Units";
+		crew="NLD_WLD_GL";
+		typicalCargo[] = {"NLD_WLD_GL"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442front.paa","\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442covered.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class NLD_DAF_Transport_Covered: I_Truck_02_covered_F
+	{
+		author="Lowlands Tactical";
+		displayName="DAF 4442 (Covered)";
+		side=1;
+		scope=2;
+		faction="NLD_Units";
+		crew="NLD_WLD_GL";
+		typicalCargo[] = {"NLD_WLD_GL"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442front.paa","\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442covered.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class NLD_DAF_Transport_Fuel: I_Truck_02_fuel_F
+	{
+		author="Lowlands Tactical";
+		displayName="DAF 4442 (Fuel)";
+		side=1;
+		scope=2;
+		faction="NLD_Units";
+		crew="NLD_WLD_GL";
+		typicalCargo[] = {"NLD_WLD_GL"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442front.paa","\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442fuel.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class NLD_DAF_Transport_repair: I_Truck_02_box_F
+	{
+		author="Lowlands Tactical";
+		displayName="DAF 4442 (repair)";
+		side=1;
+		scope=2;
+		faction="NLD_Units";
+		crew="NLD_WLD_GL";
+		typicalCargo[] = {"NLD_WLD_GL"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442front.paa","\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442repair.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class NLD_DAF_Transport_ammo: I_Truck_02_ammo_F
+	{
+		author="Lowlands Tactical";
+		displayName="DAF 4442 (Ammo)";
+		side=1;
+		scope=2;
+		faction="NLD_Units";
+		crew="NLD_WLD_GL";
+		typicalCargo[] = {"NLD_WLD_GL"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442front.paa","\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442repair.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
+
+	class NLD_DAF_Transport_medical: I_Truck_02_medical_F
+	{
+		author="Lowlands Tactical";
+		displayName="DAF 4442 (Medical)";
+		side=1;
+		scope=2;
+		faction="NLD_Units";
+		crew="NLD_WLD_CLS";
+		typicalCargo[] = {"NLD_WLD_CLS"};
+		hiddenSelectionsTextures[] = {"\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442front.paa","\NLD_Vehicle_Units\Wheeled\Daf 4442\Data\daf4442covered.paa"};
+		
+		class TransportItems {
+			class _xx_FirstAidKit {
+				name = "FirstAidKit";
+				count = 10;
+			};
+		};
+		
+		class TransportMagazines
+		{
+			class _xx_1Rnd_HE_Grenade_shell
+			{
+				magazine="1Rnd_HE_Grenade_shell";
+				count=10;
+			};
+			class _xx_HandGrenade
+			{
+				magazine="HandGrenade";
+				count=5;
+			};
+			class _xx_SmokeShell
+			{
+				magazine="SmokeShell";
+				count=10;
+			};
+			class _xx_SmokeShellGreen
+			{
+				magazine="SmokeShellGreen";
+				count=5;
+			};
+			class _xx_30Rnd_65x39_caseless_mag
+			{
+				magazine="30Rnd_65x39_caseless_mag";
+				count=25;
+			};
+			class _xx_100Rnd_65x39_caseless_mag
+			{
+				magazine="100Rnd_65x39_caseless_mag";
+				count=10;
+			};
+			class _xx_200Rnd_65x39_cased_Box
+			{
+				magazine="200Rnd_65x39_cased_Box";
+				count=10;
+			};
+			class _xx_NLAW_F
+			{
+				magazine="NLAW_F";
+				count=5;
+			};
+		};
+		class TransportWeapons {};
+	};
 };
