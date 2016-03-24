f4a6b145f2283d9031945b4ddd6725f4dbf49c1b
ancorb1974 <ancorb1974@live.nl>
2016-03-24 01:15:37 +0100

    Added TFAR backpacks
    changed changelog & classnames

---
+++ Optional/NLD_TFAR/config.cpp
@@ -0,0 +1,67 @@
+class CfgPatches
+{
+	class NLD_TFAR_Backpacks
+	{
+		units[] = {"NLD_rt1523g_Woodland","NLD_rt1523g_Desert","NLD_rt1523g_MTP","NLD_rt1523g_KM"};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"A3_Modules_F","A3_UI_F","A3_Structures_F_Items_Electronics","A3_Weapons_F_ItemHolders"};	
+	};
+};
+
+class CfgVehicles
+{
+	class tf_rt1523g;
+
+	class NLD_rt1523g_Woodland: tf_rt1523g
+	{
+		dlc="NLD_Units";
+		author="Lowlands Tactical";
+		displayName = "[NLD] RT-1523G (ASIP) Woodland";
+		descriptionShort = "RT-1523G (ASIP) NLD Woodland long range radio 20km";
+		maximumLoad = 160;
+		mass = 120;
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"NLD_TFAR\data\TFAR_LR_Woodland_co.paa"};
+		model = "\task_force_radio_items\models\clf_nicecomm2";
+	};
+
+	class NLD_rt1523g_Desert: tf_rt1523g
+	{
+		dlc="NLD_Units";
+		author="Lowlands Tactical";
+		displayName = "[NLD] RT-1523G (ASIP) Desert";
+		descriptionShort = "RT-1523G (ASIP) NLD Desert long range radio 20km";
+		maximumLoad = 160;
+		mass = 120;
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"NLD_TFAR\data\TFAR_LR_Desert_co.paa"};
+		model = "\task_force_radio_items\models\clf_nicecomm2";
+	};
+	
+	class NLD_rt1523g_MTP: tf_rt1523g
+	{
+		dlc="NLD_Units";
+		author="Lowlands Tactical";
+		displayName = "[NLD] RT-1523G (ASIP) MTP";
+		descriptionShort = "RT-1523G (ASIP) NLD MTP long range radio 20km";
+		maximumLoad = 160;
+		mass = 120;
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"NLD_TFAR\data\TFAR_LR_MTP_co.paa"};
+		model = "\task_force_radio_items\models\clf_nicecomm2";
+	};
+	
+	class NLD_rt1523g_KM: tf_rt1523g
+	{
+		dlc="NLD_Units";
+		author="Lowlands Tactical";
+		displayName = "[NLD] RT-1523G (ASIP) KM";
+		descriptionShort = "RT-1523G (ASIP) NLD KM long range radio 20km";
+		maximumLoad = 160;
+		mass = 120;
+		hiddenSelections[] = {"camo"};
+		hiddenSelectionsTextures[] = {"NLD_TFAR\data\TFAR_LR_KM_co.paa"};
+		model = "\task_force_radio_items\models\clf_nicecomm2";
+	};
+};
