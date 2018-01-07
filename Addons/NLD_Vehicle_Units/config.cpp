53b6bd9523b4fd52cdd99d671907dfd0bbb38459
Sacha Ligthert <sacha@ligthert.net>
2018-01-07 13:45:38 +0100

    Required for makepbo ( or else it wouldn't work).

---
+++ Addons/NLD_Vehicle_Units/config.cpp
@@ -0,0 +1,8 @@
+class CfgPatches {
+	class NLD_Vehicle_Units {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Characters_F_BLUFOR","NLD_Units_Gear","NLD_Units_Backpacks","NLD_Units_Insignia","NLD_Infantry_Units"};
+	};
+};
